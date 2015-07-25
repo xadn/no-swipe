8a93466a6d29a5ea2124edb4a409defeffe77185
Andy Niccolai <pair+aniccolai@pivotal.io>
2015-07-24 18:20:08 -0600

    unwrap for webpack

--- noSwipe.js
+++ noSwipe.js
@@ -1,15 +1,5 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.noSwipe = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/**
-* No Swipes v0.0.1
-*  Attach to the wheel event to prevent forward & back swipe gestures.
-*
-* Copyright 2014, Andy Niccolai
-* Licensed under the MIT license.
-*  https://github.com/xadn/no-swipe/blob/master/LICENSE
-*/
-'use strict';
-
 function noSwipe(e) {
+  'use strict';
   var el = e.target;
   var dX = e.deltaX;
   var dY = e.deltaY;
@@ -71,7 +61,6 @@ function noSwipe(e) {
   e.preventDefault();
 }
 
-module.exports = noSwipe;
-
-},{}]},{},[1])(1)
-});
+try {
+  module.exports = noSwipe;
+} catch (e) {}
